c12b1372ac8e21209c8935f7b587047a8e5870e7
Angelo <angelo-sc1@hotmail.com>
2020-08-31 11:10:16 -0300

    Verificação de mensagens de erro

--- src/routes/cadastroDeReserva.js
+++ src/routes/cadastroDeReserva.js
@@ -8,133 +8,115 @@ const Reserva = require('../database/models/Reserva')
 routes.post('/', async (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
 
-    const dados = {
-        nome: req.body.nome,
-        cpf: req.body.cpf,
-        num_passaporte: req.body.numPassaporte,
-        cep: req.body.cep,
-        logradouro: req.body.logradouro,
-        numero: req.body.numero,
-        cidade: req.body.cidade,
-        estado: req.body.estado,
-        complemento: req.body.complemento,
-        telefone: req.body.telefone,
-        quant_adultos: req.body.quantAdultos,
-        quant_criancas: req.body.quantCriancas,
-        data_inicio: req.body.dataInicio,
-        data_fim: req.body.dataFim,
-        senha: null,
-        tipoDeQuarto: req.body.tipoDeQuarto,
-        idTipoDeQuarto: null,
+    try {
+        const dados = {
+            nome: req.body.nome,
+            cpf: req.body.cpf,
+            num_passaporte: req.body.numPassaporte,
+            cep: req.body.cep,
+            logradouro: req.body.logradouro,
+            numero: req.body.numero,
+            cidade: req.body.cidade,
+            estado: req.body.estado,
+            complemento: req.body.complemento,
+            telefone: req.body.telefone,
+            quant_adultos: req.body.quantAdultos,
+            quant_criancas: req.body.quantCriancas,
+            data_inicio: req.body.dataInicio,
+            data_fim: req.body.dataFim,
+            senha: null,
+            tipoDeQuarto: req.body.tipoDeQuarto,
+            idTipoDeQuarto: null,
+        }
+    
+        const tipoDeQuarto = await TipoDeQuarto.findOne({
+            where: {
+                nome: dados.tipoDeQuarto,
+            }
+        })
+    
+        dados.idTipoDeQuarto = tipoDeQuarto.dataValues.id
+    
+        const [hospede, booleanoHospede] = await Hospede.findOrCreate({
+            where: {
+                cpf: dados.cpf
+            },
+            defaults: {
+                nome: dados.nome,
+                cpf: dados.cpf,
+                telefone: dados.telefone,
+                num_passaporte: dados.num_passaporte,
+                quant_adultos: dados.quant_adultos,
+                quant_criancas: dados.quant_criancas
+            }
+        })
+    
+        const [endereco, booleanoEndereco] = await Endereco.findOrCreate({
+            where: {
+                cep: dados.cep,
+                logradouro: dados.logradouro,
+                numero: dados.numero,
+                cidade: dados.cidade,
+                estado: dados.estado,
+                complemento: dados.complemento,
+                hospede_id: hospede.dataValues.id
+            },
+            defaults: {
+                cep: dados.cep,
+                logradouro: dados.logradouro,
+                numero: dados.numero,
+                cidade: dados.cidade,
+                estado: dados.estado,
+                complemento: dados.complemento,
+                hospede_id: hospede.dataValues.id
+            }
+        })
+    
+        const quarto = await Quarto.findOne({
+            where: {
+                tipo_de_quarto_id: dados.idTipoDeQuarto,
+            }
+        })
+    
+        const [reserva, booleanoReserva] = await Reserva.findOrCreate({
+            where: {
+                data_inicio: dados.data_inicio,
+                data_fim: dados.data_fim
+            },
+            defaults: {
+                data_inicio: dados.data_inicio,
+                data_fim: dados.data_fim,
+                senha: dados.senha,
+                hospede_id: hospede.id,
+                quarto_id: quarto.id
+            }
+        })
+    
+        res.json({
+            status: "Sucesso",
+            dados: reserva,
+        })
+        
+    } catch (err) {
+        try{
+
+            let error = "";
+
+            err.errors.forEach(erro => {
+                error += `\n${erro.message}`
+            });
+
+            if(error === "")
+                return res.status(400).json({status: "Erro", dados: "Verifique as informações e tente novamente"})
+
+            return res.status(400).json({status: "Erro", dados: error})
+        }
+        
+        catch(e){
+            return res.status(400).json({status: "Erro", dados: "Verifique as informações e tente novamente"})
+        }
     }
 
-    const tipoDeQuarto = await TipoDeQuarto.findOne({
-        where: {
-            nome: dados.tipoDeQuarto,
-        }
-    }).catch(erro => {
-        res.json({
-            status: "Erro",
-            dados: {
-                tabela: "TipoDeQuarto",
-                erro,
-            }
-        })
-    })
-
-    dados.idTipoDeQuarto = tipoDeQuarto.dataValues.id
-
-    const [hospede, booleanoHospede] = await Hospede.findOrCreate({
-        where: {
-            cpf: dados.cpf
-        },
-        defaults: {
-            nome: dados.nome,
-            cpf: dados.cpf,
-            telefone: dados.telefone,
-            num_passaporte: dados.num_passaporte,
-            quant_adultos: dados.quant_adultos,
-            quant_criancas: dados.quant_criancas
-        }
-    }).catch(erro => {
-        res.json({
-            status: "Erro",
-            dados: {
-                tabela: "Hospede",
-                erro,
-            }
-        })
-    })
-
-    const [endereco, booleanoEndereco] = await Endereco.findOrCreate({
-        where: {
-            cep: dados.cep,
-            logradouro: dados.logradouro,
-            numero: dados.numero,
-            cidade: dados.cidade,
-            estado: dados.estado,
-            complemento: dados.complemento,
-            hospede_id: hospede.dataValues.id
-        },
-        defaults: {
-            cep: dados.cep,
-            logradouro: dados.logradouro,
-            numero: dados.numero,
-            cidade: dados.cidade,
-            estado: dados.estado,
-            complemento: dados.complemento,
-            hospede_id: hospede.dataValues.id
-        }
-    }).catch(erro => {
-        res.json({
-            status: "Erro",
-            dados: {
-                tabela: "Endereco",
-                erro,
-            }
-        })
-    })
-
-    const quarto = await Quarto.findOne({
-        where: {
-            tipo_de_quarto_id: dados.idTipoDeQuarto,
-        }
-    }).catch(erro => {
-        res.json({
-            status: "Erro",
-            dados: {
-                tabela: "Quarto",
-                erro,
-            }
-        })
-    })
-
-    const [reserva, booleanoReserva] = await Reserva.findOrCreate({
-        where: {
-            data_inicio: dados.data_inicio,
-            data_fim: dados.data_fim
-        },
-        defaults: {
-            data_inicio: dados.data_inicio,
-            data_fim: dados.data_fim,
-            senha: dados.senha,
-            hospede_id: hospede.id,
-            quarto_id: quarto.id
-        }
-    }).catch(erro => {
-        res.json({
-            status: "Erro",
-            dados: {
-                tabela: "Reserva",
-                erro,
-            }
-        })
-    })
-
-    res.json({
-        status: "Sucesso",
-        dados: reserva,
-    })
 })
 
 module.exports = routes
